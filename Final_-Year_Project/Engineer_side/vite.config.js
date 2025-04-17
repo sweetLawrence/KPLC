c2388e074f240d877091d5a98f7982473947ffa4
Lawrence <tsungulawrence@gmail.com>
2025-04-17 19:21:53 +0300

    code

--- Final_-Year_Project/Engineer_side/vite.config.js
+++ Final_-Year_Project/Engineer_side/vite.config.js
@@ -4,16 +4,10 @@
 
 // import { VitePWA } from 'vite-plugin-pwa'
 
-
 // // https://vite.dev/config/
 // export default defineConfig({
 //   plugins: [ tailwindcss(), react() ],
 // })
-
-
-
-
-
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
@@ -35,16 +29,19 @@ export default defineConfig({
         background_color: '#ffffff',
         display: 'standalone',
         start_url: '/',
+        
         icons: [
           {
             src: '/web-app-manifest-192x192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any'
           },
           {
             src: '/web-app-manifest-512x512.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any'
           },
           {
             src: '/apple-touch-icon.png',
