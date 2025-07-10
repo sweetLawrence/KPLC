5a92cfceb3c30dc68c6c5b0e4850382b0a8ef998
Lawrence <tsungulawrence@gmail.com>
2025-07-11 00:11:18 +0300

    code

--- Backend/routes/permits/permit-approval.js
+++ Backend/routes/permits/permit-approval.js
@@ -3,23 +3,21 @@ const router = express.Router()
 const { Permit } = require('../../Database/models')
 
 router.post('/approve-permit/:permitId', async (req, res) => {
-  const { permitId } = req.params 
+  const { permitId } = req.params
   const { permitPayload } = req.body
 
   console.log(
     '----------------Received APPROVE permitId---------------:',
     permitId
-  ) 
+  )
   console.log(permitPayload)
 
   try {
-   
     const permit = await Permit.findOne({
       where: { id: permitId }
     })
 
     if (permit) {
-   
       permit.status = 'approved'
       permit.comments = permitPayload.comments
       permit.approverName = permitPayload.approverName
@@ -33,39 +31,43 @@ router.post('/approve-permit/:permitId', async (req, res) => {
         permitStatus: 'approved'
       })
     } else {
-      
       return res.status(404).json({ message: 'Permit not found' })
     }
   } catch (error) {
-    console.error(error) 
+    console.error(error)
     return res.status(500).json({ message: 'Internal server error' })
   }
 })
 
 router.post('/reject-permit/:permitId', async (req, res) => {
-  const { permitId } = req.params 
+  const { permitId } = req.params
   const { permitPayload } = req.body
   console.log(permitPayload)
 
   console.log(
     '----------------Received REJECT permitId---------------:',
     permitId
-  ) 
+  )
 
   try {
-    
     const permit = await Permit.findOne({
       where: { id: permitId }
     })
 
     if (permit) {
-      
       permit.status = 'rejected'
       permit.comments = permitPayload.comments
       permit.approverName = permitPayload.approverName
       permit.issueDate = permitPayload.issueDate
       permit.issueTime = permitPayload.issueTime
-      await permit.save() 
+      await permit.save()
+
+      res.clearCookie('auth_token', {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'Strict',
+        path: '/'
+      })
 
       return res.json({
         message: 'p-failure',
@@ -74,11 +76,10 @@ router.post('/reject-permit/:permitId', async (req, res) => {
         rejectionReason: permitPayload.comments
       })
     } else {
-
       return res.status(404).json({ message: 'Permit not found' })
     }
   } catch (error) {
-    console.error(error) 
+    console.error(error)
     return res.status(500).json({ message: 'Internal server error' })
   }
 })
